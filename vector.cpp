a74d66c76fc504a5e5e64d811cc1e15ae0dbd42b
Coffee <142932792+Sergeras1@users.noreply.github.com>
2024-05-07 14:14:35 +0500

    добавлен файл cpp с созданием треугольников

---
+++ vector.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+    int height;
+    cout << "Enter the height for the triangle: " << endl;
+    cin >> height;
+    for (int i = 1; i <= height; ++i) {
+        for (int j = 1; j <= height - i; ++j) {
+        cout << " ";
+        }
+        for (int j = 1; j <= 2 * i; ++j) {
+            cout << "*";
+        }
+        cout << endl;
+    }
+
+    for (int i = 1; i <= height; i++) {
+        cout << endl;
+        for (int j = 1; j <= i; j++) {
+            cout << "*";
+        }
+    }
+}
+
